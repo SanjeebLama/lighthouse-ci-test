e33745649fb6ad4854f7af403723b4915565ade8
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-04-20 14:50:01 +0545

    test #C13

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	ci: {
 		collect: {
-			url: [process.env.PREVIEW_URL],
+			url: [`https://${process.env.PREVIEW_URL}`],
 			startServerCommand: process.env.NODE_ENV === 'production' ? undefined : 'npm run start',
 		},
 		assert: {
