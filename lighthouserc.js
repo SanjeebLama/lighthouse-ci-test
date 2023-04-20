cff0e100a359107cdca7a1efa955aec347d3e6b6
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-04-20 16:34:37 +0545

    test #C20

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,18 @@
 module.exports = {
 	ci: {
 		collect: {
-			url: [`https://${process.env.PREVIEW_URL}`],
+			url: process.env.NODE_ENV === 'production' ? `https://${process.env.PREVIEW_URL}` : ['http://localhost:3000/'],
 			startServerCommand: process.env.NODE_ENV === 'production' ? undefined : 'npm run start',
 		},
 		assert: {
 			// preset: 'lighthouse:recommended',
 			preset: 'lighthouse:no-pwa',
+			// skipAudits:
+			assertions: {
+				'bf-cache': 'off',
+				'csp-xss': 'off',
+				'unused-javascript': 'off',
+			},
 		},
 		upload: {
 			target: 'temporary-public-storage',
