59f4aa226035b15fa29e3d64e7f8662382841b0d
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-04-21 12:39:24 +0545

    test #N8 with about page

--- lighthouserc.js
+++ lighthouserc.js
@@ -11,6 +11,11 @@ module.exports = {
 			// url: ['http://localhost:3000','http://localhost:3000/about'],
 			startServerCommand:
 				process.env.NODE_ENV === 'production' ? undefined : 'npm run start',
+
+			// settings: {
+			// 	// Don't run certain audits
+			// 	skipAudits: ['redirects-http'],
+			// },
 		},
 		assert: {
 			// preset: 'lighthouse:recommended',
